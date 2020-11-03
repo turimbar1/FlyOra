7448aa96bb6241a9f69c42e4859d51d3a1aa55d2
WIN2016\redgate <redgate@redgate.com>
2020-11-03 22:31:25 +0000

    cleaning cleaning

--- sql/Batch.bat
+++ sql/Batch.bat
@@ -1,20 +1,14 @@
-echo ===================================================================================================== 
-echo "====================Capturing schema state and log history before deployment===================="
-echo =====================================================================================================
- 
+
+echo "Capturing schema state and log history before deployment"
 "C:\Program Files\Red Gate\Schema Compare for Oracle 5\sco.exe" /source HR/Redgate1@localhost/dev1_shadow{HR} /target C:\VCSRepositories\OracleState{HR} /deploy /indirect
 "C:\Program Files\Red Gate\Data Compare for Oracle 5\dco.exe" /source HR/Redgate1@localhost/dev1_shadow{HR} /target C:\VCSRepositories\OracleState{HR} /deploy /indirect
 
-echo ===================================================================================================== 
-echo "=================================== Deploying Changes ========================================"
-echo =====================================================================================================
 
+echo "Deploying Changes"
 flyway migrate 
 
-echo ===================================================================================================== 
-echo "====================================== Rolling back ==============================================="
-echo =====================================================================================================
-
-
+echo "Rolling back"
 "C:\Program Files\Red Gate\Schema Compare for Oracle 5\sco.exe" /target HR/Redgate1@localhost/dev1_shadow{HR} /source C:\VCSRepositories\OracleState{HR} /deploy /indirect
 "C:\Program Files\Red Gate\Data Compare for Oracle 5\dco.exe" /target HR/Redgate1@localhost/dev1_shadow{HR} /source C:\VCSRepositories\OracleState{HR} /deploy /indirect
+
+echo "Finished"
